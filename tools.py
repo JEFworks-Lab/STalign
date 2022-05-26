dc7bef71b340e765dccc4e3a9ed4d99d8d9f095c
Daniel Tward <daniel.tward@gmail.com>
2022-05-26 10:28:37 -0700

    tools.py
    working lddmm and fast rasterization but no points

--- tools.py
+++ tools.py
@@ -9,7 +9,7 @@ from torch.nn.functional import grid_sample
 
 
 
-def rasterize(x, y, dx=30.0, blur=1.0, expand=1.1, draw=0, wavelet_magnitude=False):
+def rasterize(x, y, dx=30.0, blur=1.0, expand=1.1, draw=0, wavelet_magnitude=False,use_windowing=True):
     ''' Rasterize a spatial transcriptomics dataset into a density image
     
     Paramters
@@ -59,6 +59,10 @@ def rasterize(x, y, dx=30.0, blur=1.0, expand=1.1, draw=0, wavelet_magnitude=Fal
     Rasterize a dataset at 30 micron pixel size, with three kernels, using difference between scales.
     
     >>> X,Y,M,fig = tools.rasterize(x,y,dx=30.0,blur=[2.0,1.0,0.5],draw=10000, wavelet_magnitude=True)
+    
+    TODO
+    ----
+    Use a window to make kernels faster and verify it is working. Note it looks like part is chopped off
         
     '''
     
@@ -68,6 +72,9 @@ def rasterize(x, y, dx=30.0, blur=1.0, expand=1.1, draw=0, wavelet_magnitude=Fal
     nb = len(blur)
     blur = np.array(blur)
     n = len(x)
+    maxblur = np.max(blur) # for windowing
+    
+    
     
     if wavelet_magnitude and np.any(blur != np.sort(blur)[::-1]):
         raise Exception('When using wavelet magnitude, blurs must be sorted from greatest to least')
@@ -81,7 +88,7 @@ def rasterize(x, y, dx=30.0, blur=1.0, expand=1.1, draw=0, wavelet_magnitude=Fal
     X_ = np.arange(minx,maxx,dx)
     Y_ = np.arange(miny,maxy,dx)
     
-    X = np.stack(np.meshgrid(X_,Y_))
+    X = np.stack(np.meshgrid(X_,Y_)) # note this is xy order, not row col order
 
     W = np.zeros((X.shape[1],X.shape[2],nb))
 
@@ -103,16 +110,41 @@ def rasterize(x, y, dx=30.0, blur=1.0, expand=1.1, draw=0, wavelet_magnitude=Fal
         # but doesn't use a kernel
         # I[c_,row,col] += 1.0
         # W[row,col] += 1.0
+        if not use_windowing: # legacy version
+            k = np.exp( - ( (X[0][...,None] - x_)**2 + (X[1][...,None] - y_)**2 )/(2.0*(dx*blur*2)**2)  )
+            k /= np.sum(k,axis=(0,1),keepdims=True)     
+            if wavelet_magnitude:
+                for i in reversed(range(nb)):
+                    if i == 0:
+                        continue
+                    k[...,i] = k[...,i] - k[...,i-1]
 
-        k = np.exp( - ( (X[0][...,None] - x_)**2 + (X[1][...,None] - y_)**2 )/(2.0*(dx*blur*2)**2)  )
-        k /= np.sum(k,axis=(0,1),keepdims=True)     
-        if wavelet_magnitude:
-            for i in reversed(range(nb)):
-                if i == 0:
-                    continue
-                k[...,i] = k[...,i] - k[...,i-1]
-        
-        W += k
+            W += k
+        else: # use a small window
+            r = int(np.ceil(maxblur*4))
+            col = np.round((x_ - X_[0])/dx).astype(int)
+            row = np.round((y_ - Y_[0])/dx).astype(int)
+            
+            row0 = np.floor(row-r).astype(int)
+            row1 = np.ceil(row+r).astype(int)                    
+            col0 = np.floor(col-r).astype(int)
+            col1 = np.ceil(col+r).astype(int)
+            # we need boundary conditions
+            row0 = np.minimum(np.maximum(row0,0),W.shape[0]-1)
+            row1 = np.minimum(np.maximum(row1,0),W.shape[0]-1)
+            col0 = np.minimum(np.maximum(col0,0),W.shape[1]-1)
+            col1 = np.minimum(np.maximum(col1,0),W.shape[1]-1)
+            
+           
+            k = np.exp( - ( (X[0][row0:row1+1,col0:col1+1,None] - x_)**2 + (X[1][row0:row1+1,col0:col1+1,None] - y_)**2 )/(2.0*(dx*blur*2)**2)  )
+            k /= np.sum(k,axis=(0,1),keepdims=True)     
+            if wavelet_magnitude:
+                for i in reversed(range(nb)):
+                    if i == 0:
+                        continue
+                    k[...,i] = k[...,i] - k[...,i-1]
+            W[row0:row1+1,col0:col1+1,:] += k
+            
         
             
         
@@ -310,8 +342,9 @@ def extent_from_x(xJ):
 def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
           L=None,T=None,A=None,
           a=500.0,p=3.0,expand=2.0,nt=3,
-         niter=5000,diffeo_start=100, epL=5e-11, epT=5e-4, epV=5e1,
-         sigmaM=0.2,sigmaB=0.19,sigmaA=0.3,sigmaR=5e5,sigmaP=2e-1):
+         niter=5000,diffeo_start=100, epL=1e-10, epT=1e-3, epV=5e1,
+         sigmaM=1.0,sigmaB=2.0,sigmaA=5.0,sigmaR=5e5,sigmaP=2e-1,
+         device='cpu',dtype=torch.float64):
     ''' Run LDDMM between a pair of images.
     
     This jointly estimates an affine transform A, and a diffeomorphism phi.
@@ -376,8 +409,10 @@ def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
     sigmaP: float
         Standard deviation for matching of points.  
         Cost is of the form 0.5/sigmaP^2 sum_i (atlas_point_i - target_point_i)^2
-        
-        
+    device: str
+        torch device. defaults to 'cpu'. Can also be 'cuda:0' for example.
+    dtype: torch dtype
+        torch data type. defaults to torch.float64
     
     Returns
     -------
@@ -389,6 +424,8 @@ def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
     TODO
     ----
     Include input for initialization.
+    
+    Include a metric for L and T
     
     '''
     
@@ -408,17 +445,27 @@ def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
 
     # check initial inputs
     if A is not None:
+        # if we specify an A
         if L is not None or T is not None:
             raise Exception('If specifying A, you must not specify L or T')
-        A
+        L = torch.tensor(A[:3,:3],device=device,dtype=dtype,requires_grad=True)
+        T = torch.tensor(A[:3,-1],device=device,dtype=dtype,requires_grad=True)   
+    else:
+        # if we do not specify A                
+        if L is None: L = torch.eye(2,device=device,dtype=dtype,requires_grad=True)
+        if T is None: T = torch.zeros(2,device=device,dtype=dtype,requires_grad=True)
+    L = torch.tensor(L,device=device,dtype=dtype,requires_grad=True)
+    T = torch.tensor(T,device=device,dtype=dtype,requires_grad=True)
+    # change to torch
+    I = torch.tensor(I,device=device,dtype=dtype)                         
+    J = torch.tensor(J,device=device,dtype=dtype)
     
-    
-    L = torch.eye(2,device=device,dtype=dtype,requires_grad=True)
-    #L.data[0,0] = -1.0
-    #L.data[:2,:2] *= 0.2
-    T = torch.zeros(2,device=device,dtype=dtype,requires_grad=True)
-    #T.data[0] = +2000.0
-    #T.data[1] = -xI[1][-1]/2.0*1.1
+    #L = torch.eye(2,device=device,dtype=dtype,requires_grad=True)
+    ##L.data[0,0] = -1.0
+    ##L.data[:2,:2] *= 0.2
+    #T = torch.zeros(2,device=device,dtype=dtype,requires_grad=True)
+    ##T.data[0] = +2000.0
+    ##T.data[1] = -xI[1][-1]/2.0*1.1
 
     # velocity
     #a = 500.0
@@ -437,25 +484,45 @@ def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
     LL = (1.0 + 2.0*a**2* torch.sum( (1.0 - torch.cos(2.0*np.pi*FV*dv))/dv**2 ,-1))**(p*2.0)
 
     K = 1.0/LL
-    fig,ax = plt.subplots()
-    ax.imshow(K,vmin=0.0,vmax=0.1,extent=extentF)
+    #fig,ax = plt.subplots()
+    #ax.imshow(K,vmin=0.0,vmax=0.1,extent=extentF)
+    
     #fig,ax = plt.subplots()
     #ax.imshow(K[0].cpu())
     DV = torch.prod(dv)
     Ki = torch.fft.ifftn(K).real
     fig,ax = plt.subplots()
     ax.imshow(Ki,vmin=0.0,extent=extentV)
+    ax.set_title('smoothing kernel')
+    fig.canvas.draw()
 
 
     # nt = 3
     v = torch.zeros((nt,XV.shape[0],XV.shape[1],XV.shape[2]),device=device,dtype=dtype,requires_grad=True)
 
 
-    WM = torch.ones(Jd[0].shape,dtype=J.dtype,device=J.device)*0.5
-    WB = torch.ones(Jd[0].shape,dtype=J.dtype,device=J.device)*0.4
-    WA = torch.ones(Jd[0].shape,dtype=J.dtype,device=J.device)*0.1
-    pointsI = torch.tensor(pointsI,device=J.device,dtype=J.dtype)
-    pointsJ = torch.tensor(pointsJ,device=J.device,dtype=J.dtype)
+    WM = torch.ones(J[0].shape,dtype=J.dtype,device=J.device)*0.5
+    WB = torch.ones(J[0].shape,dtype=J.dtype,device=J.device)*0.4
+    WA = torch.ones(J[0].shape,dtype=J.dtype,device=J.device)*0.1
+    if pointsI is None and pointsJ is None:
+        pointsI = torch.zeros((0,2),device=J.device,dtype=J.dtype)
+        pointsJ = torch.zeros((0,2),device=J.device,dtype=J.dtype) 
+    elif (pointsI is None and pointsJ is not None) or (pointsJ is None and pointsI is not None):
+        raise Exception('Must specify corresponding sets of points or none at all')
+    else:
+        pointsI = torch.tensor(pointsI,device=J.device,dtype=J.dtype)
+        pointsJ = torch.tensor(pointsJ,device=J.device,dtype=J.dtype)
+    
+    
+    xI = [torch.tensor(x,device=device,dtype=dtype) for x in xI]
+    xJ = [torch.tensor(x,device=device,dtype=dtype) for x in xJ]
+    XI = torch.stack(torch.meshgrid(*xI,indexing='ij'),-1)
+    XJ = torch.stack(torch.meshgrid(*xJ,indexing='ij'),-1)
+    dJ = [x[1]-x[0] for x in xJ]
+    extentJ = (xJ[1][0].item()-dJ[1]/2.0,
+          xJ[1][-1].item()+dJ[1]/2.0,
+          xJ[0][-1].item()+dJ[0]/2.0,
+          xJ[0][0].item()-dJ[0]/2.0)
     
     #sigmaM = 0.2
     #sigmaB = 0.19
@@ -482,15 +549,16 @@ def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
         # Ai
         Ai = torch.linalg.inv(A)
         # transform sample points
-        Xs = (Ai[:2,:2]@XJd[...,None])[...,0] + Ai[:2,-1]    
+        Xs = (Ai[:2,:2]@XJ[...,None])[...,0] + Ai[:2,-1]    
         # now diffeo, not semilagrange here
         for t in range(nt-1,-1,-1):
             Xs = Xs + interp(xv,-v[t].permute(2,0,1),Xs.permute(2,0,1)).permute(1,2,0)/nt
         # and points
         pointsIt = torch.clone(pointsI)
-        for t in range(nt):
-            pointsIt += interp(xv,v[t].permute(2,0,1),pointsIt.T[...,None])[...,0].T/nt
-        pointsIt = (A[:2,:2]@pointsIt.T + A[:2,-1][...,None]).T
+        if pointsIt.shape[0] >0:
+            for t in range(nt):            
+                pointsIt += interp(xv,v[t].permute(2,0,1),pointsIt.T[...,None])[...,0].T/nt
+            pointsIt = (A[:2,:2]@pointsIt.T + A[:2,-1][...,None]).T
 
         # transform image
         AI = interp(xI,I,Xs.permute(2,0,1))
@@ -508,25 +576,31 @@ def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
         #B[9] = (AI[1][None]*AI[2][None]).reshape(1,-1)
         with torch.no_grad():    
             BB = B@(B*WM.ravel()).T
-            BJ = B@((Jd*WM).reshape(Jd.shape[0],Jd.shape[1]*Jd.shape[2])).T
+            BJ = B@((J*WM).reshape(J.shape[0],J.shape[1]*J.shape[2])).T
             small = 0.1
             coeffs = torch.linalg.solve(BB + small*torch.eye(BB.shape[0],device=BB.device,dtype=BB.dtype),BJ)
-        fAI = ((B.T@coeffs).T).reshape(Jd.shape)
+        fAI = ((B.T@coeffs).T).reshape(J.shape)
 
         # objective function
-        EM = torch.sum((fAI - Jd)**2*WM)/2.0/sigmaM**2
+        EM = torch.sum((fAI - J)**2*WM)/2.0/sigmaM**2
         ER = torch.sum(torch.sum(torch.abs(torch.fft.fftn(v,dim=(1,2)))**2,dim=(0,-1))*LL)*DV/2.0/v.shape[1]/v.shape[2]/sigmaR**2
-        EP = torch.sum((pointsIt - pointsJ)**2)/2.0/sigmaP**2
-        E = EM + ER + EP
-        Esave.append( (E.item(), EM.item(), ER.item(), EP.item()) )
+        E = EM + ER
+        tosave = [E.item(), EM.item(), ER.item()]
+        if pointsIt.shape[0]>0:
+            EP = torch.sum((pointsIt - pointsJ)**2)/2.0/sigmaP**2
+            E += EP
+            tosave.append(EP.item())
+        
+        Esave.append( tosave )
         # gradient update
         E.backward()
-        with torch.no_grad():
+        with torch.no_grad():            
             L -= (epL/(1.0 + (it>=diffeo_start)*9))*L.grad
             T -= (epT/(1.0 + (it>=diffeo_start)*9))*T.grad
 
             L.grad.zero_()
             T.grad.zero_()
+            
 
             # v grad
             vgrad = v.grad
@@ -541,11 +615,11 @@ def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
         if not it%5:
             with torch.no_grad():
                 # M step for these params
-                muA = torch.sum(WA*Jd,dim=(-1,-2))/torch.sum(WA)
-                muB = torch.sum(WB*Jd,dim=(-1,-2))/torch.sum(WB)
+                muA = torch.sum(WA*J,dim=(-1,-2))/torch.sum(WA)
+                muB = torch.sum(WB*J,dim=(-1,-2))/torch.sum(WB)
                 if it <= 200:
                     muA = torch.tensor([0.75,0.77,0.79],device=J.device,dtype=J.dtype)
-                    muB = torch.ones(Jd.shape[0],device=J.device,dtype=J.dtype)*0.96
+                    muB = torch.ones(J.shape[0],device=J.device,dtype=J.dtype)*0.96
 
                 if it >= 50:
 
@@ -556,9 +630,9 @@ def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
 
 
                     # now the E step, update the weights
-                    WM = pi[0]* torch.exp( -torch.sum((fAI - Jd)**2,0)/2.0/sigmaM**2 )/np.sqrt(2.0*np.pi*sigmaM**2)**J.shape[0]
-                    WA = pi[1]* torch.exp( -torch.sum((muA[...,None,None] - Jd)**2,0)/2.0/sigmaA**2 )/np.sqrt(2.0*np.pi*sigmaA**2)**J.shape[0]
-                    WB = pi[2]* torch.exp( -torch.sum((muB[...,None,None] - Jd)**2,0)/2.0/sigmaB**2 )/np.sqrt(2.0*np.pi*sigmaB**2)**J.shape[0]
+                    WM = pi[0]* torch.exp( -torch.sum((fAI - J)**2,0)/2.0/sigmaM**2 )/np.sqrt(2.0*np.pi*sigmaM**2)**J.shape[0]
+                    WA = pi[1]* torch.exp( -torch.sum((muA[...,None,None] - J)**2,0)/2.0/sigmaA**2 )/np.sqrt(2.0*np.pi*sigmaA**2)**J.shape[0]
+                    WB = pi[2]* torch.exp( -torch.sum((muB[...,None,None] - J)**2,0)/2.0/sigmaB**2 )/np.sqrt(2.0*np.pi*sigmaB**2)**J.shape[0]
                     WS = WM+WB+WA
                     WS += torch.max(WS)*1e-6
                     WM /= WS
@@ -574,16 +648,16 @@ def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
             ax[0].imshow(((AI-torch.amin(AI,(1,2))[...,None,None])/(torch.amax(AI,(1,2))-torch.amin(AI,(1,2)))[...,None,None]).permute(1,2,0).clone().detach().cpu(),extent=extentJ)
             ax[0].scatter(pointsIt[:,1].clone().detach().cpu(),pointsIt[:,0].clone().detach().cpu())
             ax[1].cla()    
-            ax[1].imshow(clip(fAI.permute(1,2,0).clone().detach()).cpu(),extent=extentJ)
+            ax[1].imshow(clip(fAI.permute(1,2,0).clone().detach()/torch.max(J).item()).cpu(),extent=extentJ)
             ax[1].scatter(pointsIt[:,1].clone().detach().cpu(),pointsIt[:,0].clone().detach().cpu())
 
             ax[5].cla()
-            ax[5].imshow(clip(fAI - Jd).permute(1,2,0).clone().detach().cpu()*0.5+0.5,extent=extentJ)
+            ax[5].imshow(clip(fAI - J).permute(1,2,0).clone().detach().cpu()/(torch.max(J).item())*0.5+0.5,extent=extentJ)
             ax[5].scatter(pointsIt[:,1].clone().detach().cpu(),pointsIt[:,0].clone().detach().cpu())
             ax[5].scatter(pointsJ[:,1].clone().detach().cpu(),pointsJ[:,0].clone().detach().cpu())
 
             ax[2].cla()
-            ax[2].imshow(Jd.permute(1,2,0).cpu(),extent=extentJ)
+            ax[2].imshow(J.permute(1,2,0).cpu()/torch.max(J).item(),extent=extentJ)
             ax[2].scatter(pointsJ[:,1].clone().detach().cpu(),pointsJ[:,0].clone().detach().cpu())
 
             ax[4].cla()
@@ -607,4 +681,42 @@ def LDDMM(xI,I,xJ,J,pointsI=None,pointsJ=None,
             fig.canvas.draw()
             figE.canvas.draw()
             
-    return A,V
+    return A,v
+
+
+def build_transform(xv,v,A,XJ):
+    ''' Create sample points to transform atlas to target from affine and velocity.
+    
+    Parameters
+    ----------
+    xv : list of array
+        Sample points for velocity
+    v : array
+        time dependent velocity field
+    A : array
+        Affine transformation matrix
+    XJ : array
+        Sample points for target (meshgrid with ij index style)
+    
+    Returns
+    -------
+    Xs : array
+        Sample points in mehsgrid format.
+    
+    
+    TODO
+    ----
+    Check types.
+    
+    '''
+    A = torch.tensor(A)
+    v = torch.tensor(v)
+    XJ = torch.tensor(XJ)
+    Ai = torch.linalg.inv(A)
+    # transform sample points
+    Xs = (Ai[:2,:2]@XJ[...,None])[...,0] + Ai[:2,-1]    
+    # now diffeo, not semilagrange here
+    nt = v.shape[0]
+    for t in range(nt-1,-1,-1):
+        Xs = Xs + interp(xv,-v[t].permute(2,0,1),Xs.permute(2,0,1)).permute(1,2,0)/nt
+    return Xs
