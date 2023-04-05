0c7f69b9f11c4fa253b5af4366c49cfac2a38307
mmganant <manjari.anant@gmail.com>
2023-04-05 18:49:51 -0400

    change theme back

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 
