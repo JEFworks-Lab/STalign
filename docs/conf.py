c9dfc8a004c70b9c101aab1aa71be68376e1096a
mmganant <manjari.anant@gmail.com>
2023-04-06 23:12:09 -0400

    add xenium-starmap notebook to website

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser', 'autodocsumm', 'nbsphinx',
+extensions = ['myst_parser', 'autodocsumm', 'nbsphinx', 
 ]
 source_suffix = ['.rst', '.md'
 ]
