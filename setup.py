a2040493bd3d4c1a894b2ea60382ace025b7c1dc
mmganant <manjari.anant@gmail.com>
2023-04-14 12:57:56 -0400

    add requirements.txt

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open('requirements.txt') as f:
+	required = f.read().splitlines()
+
 setup(
     name='STalign',
     author='JEFworks',
     version='1.0',
     include_package_data=True,
     description='aligning spatial transcriptomics data',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires = required
 )
